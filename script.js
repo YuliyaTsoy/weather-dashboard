b9de9a2352a64c8daf32e88aa2839443662a0703
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2023-12-07 00:35:40 -0500

    added functions to display current weather conditions and 5-day forecast

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function getInput(){
     $(".city-name").append(dateTime)   
     
     var geoLink = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityCode + "," + countryCode + "&limit=5&appid=2c31d1b4f04a9808197b6b9242d8d05a"
-
+    // using fetch method, conver response into json and return
     fetch(geoLink)
     .then(function(response){
         return response.json();
@@ -70,6 +70,7 @@ function getInput(){
         longitude = data[0].lon;
         latitude = data[0].lat;
 
+        // fetching current wather
         var theUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + "&lon="+ longitude + "&exclude=minutely,hourly,alerts&units=imperial&appid=2c31d1b4f04a9808197b6b9242d8d05a";
 
         fetch(theUrl)
@@ -91,11 +92,44 @@ function getInput(){
             temperature.text("Temperature: "+ data.current.temp + " F");
             humidity.text("Humidity: " + data.current.humidity + " %");
 
+            // handle UV index
+            var uvi =$("<div>")
             uvIndex.addClass("d-flex")
             uvIndex.text("UV Index: ");
-            var uvi =$("<div>")
             uvi.text(data.current.uvi)
             uvIndex.append(uvi)
+
+            //display 5 day forecast
+
+            for (var i = 1; i<6; i++){
+            var divContainer = $("<div>")
+            this["futureHumidity"+i] = $("<div>")
+            this["futureTemp"+i] = $("<div>")
+            this["futureWind"+i] = $("<div>")
+            this["futureIcon"+i] = $("<img>")
+            this["futureDate"+i] = $("<h>")
+            // convert to date
+            this["forecastDay"+i] = new Date(data.daily[i].dt * 1000); 
+            
+            
+            (this["futureDate"+i]).text(((this["forecastDay"+i]).getMonth()+1) + "/" + (this["forecastDay"+i]).getDate() + "/" + (this["forecastDay"+i]).getFullYear());
+            (this["futureTemp"+i]).text("Temperature: "+ data.daily[i].temp.day + " F");
+            (this["futureWind"+i]).text("Wind: "+ data.daily[i].wind_speed+ " MPH");
+            (this["futureHumidity"+i]).text("Humidity: " + data.daily[i].humidity + " %");
+            (this["weatherIcon"+i])= data.daily[i].weather[0].icon;
+
+            Dateimg = "https://openweathermap.org/img/wn/" + (this["weatherIcon"+i]) + ".png";  
+                (this["futureIcon"+i]).attr('src',DateimgSrc)
+
+                $(".five-day-forecast").append(divContainer)
+                divContainer.addClass("weather")
+                divContainer.append((this["futureDate"+i]));
+                divContainer.append((this["futureIcon"+i]));
+                divContainer.append((this["futureTemp"+i]));
+                divContainer.append((this["futureWind"+i]));
+                divContainer.append((this["futureHumidity"+i]));
+
+            }
 
         })
     })
