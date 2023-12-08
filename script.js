fed2f485dbf76960910f6fee135fe2ce99209fa2
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2023-12-07 23:16:55 -0500

    corrected scrtip js code and added style css

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// var APIKey = "2c31d1b4f04a9808197b6b9242d8d05a";
+// APIKey = "04f50896a4e31a98e332740a88e3546c";
 
-// adding input to the list of previously searched cities 
 function addInput(){
     cityInput = $("#cityInput").val();
     cityList = getData();
@@ -13,17 +12,10 @@ function addInput(){
         $(".prev-searched-cities").append(lastCity)
     }
     $(".card-subtitle").attr("style","display:inline")
-    addData(cityInput);
+    addInputLocalStorage ();
+    getInput();
+    
 };
-// event listener for previously searched city
-$(".prev-searched-cities").on('click', function(event){
-    event.preventDefault();
-    $(".card-subtitle").attr("style","display:inline")
-     $("#cityInput").val() =  event.target.id;
-    getInput(); 
-});
-
-
 // showing current weather conditions for the city
 
 function getInput(){
@@ -32,8 +24,8 @@ function getInput(){
 
     cityInput = $("#cityInput").val();
     var cityCode = cityInput;
-    var countryCode = "US";
-    
+    // var countryCode = "US";
+    console.log(cityCode);
     var longitude;
     var latitude;
 
@@ -45,11 +37,11 @@ function getInput(){
     var wind = $("<div>")    
     var temperature = $("<div>")    
     var humidity = $("<div>")   
-    var uvIndex = $("<div>")  
+    
+    
 
     $(".city-name").addClass("wather-container")
     $(".city-name").append(icon)    
-    $(".city-name").append(uvIndex)
     $(".city-name").append(wind)    
     $(".city-name").append(temperature)    
     $(".city-name").append(humidity)    
@@ -75,58 +67,59 @@ function getInput(){
         })
         .then (function(data){
             console.log(data)
-            weatherIcon= data.current.weather[0].icon;
-
+           weatherIcon = data.weather[0].icon;
+            
             img = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
             icon.attr('src',img)
         
             cityName.text(cityCode);
 
-            var date = new Date(data.current.dt * 1000);
+            var date = new Date(data.dt * 1000);
+            console.log (date)
             dateTime.text("("+ (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() + ")");
 
-            wind.text("Wind Speed: " + data.current.wind_speed + " MPH");
-            temperature.text("Temperature: "+ data.current.temp + " F");
-            humidity.text("Humidity: " + data.current.humidity + " %");
+            wind.text("Wind Speed: " + data.wind.speed + " MPH");
+            temperature.text("Temperature: "+ data.main.temp + " F");
+            humidity.text("Humidity: " + data.main.humidity + " %");
 
-            // handle UV index
-            var uvi =$("<div>")
-            uvIndex.addClass("d-flex")
-            uvIndex.text("UV Index: ");
-            uvi.text(data.current.uvi)
-            uvIndex.append(uvi)
 
             //display 5 day forecast
 
-            for (var i = 1; i<6; i++){
-            var divContainer = $("<div>")
-            this["futureHumidity"+i] = $("<div>")
-            this["futureTemp"+i] = $("<div>")
-            this["futureWind"+i] = $("<div>")
-            this["futureIcon"+i] = $("<img>")
-            this["futureDate"+i] = $("<h>")
-            // convert to date
-            this["forecastDay"+i] = new Date(data.daily[i].dt * 1000); 
-            
-            
-            (this["futureDate"+i]).text(((this["forecastDay"+i]).getMonth()+1) + "/" + (this["forecastDay"+i]).getDate() + "/" + (this["forecastDay"+i]).getFullYear());
-            (this["futureTemp"+i]).text("Temperature: "+ data.daily[i].temp.day + " F");
-            (this["futureWind"+i]).text("Wind: "+ data.daily[i].wind_speed+ " MPH");
-            (this["futureHumidity"+i]).text("Humidity: " + data.daily[i].humidity + " %");
-            (this["weatherIcon"+i])= data.daily[i].weather[0].icon;
+            var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude +'&lon='+longitude+ '&appid=04f50896a4e31a98e332740a88e3546c';
 
-            Dateimg = "https://openweathermap.org/img/wn/" + (this["weatherIcon"+i]) + ".png";  
-                (this["futureIcon"+i]).attr('src',DateimgSrc)
-
-                $(".five-day-forecast").append(divContainer)
-                divContainer.addClass("weather")
-                divContainer.append((this["futureDate"+i]));
-                divContainer.append((this["futureIcon"+i]));
-                divContainer.append((this["futureTemp"+i]));
-                divContainer.append((this["futureWind"+i]));
-                divContainer.append((this["futureHumidity"+i]));
-
-            }
+            fetch(forecastURL)
+            .then (function(response){
+                return response.json();
+            })
+            .then (function(data){
+                console.log(data.list);
+                 // for loop that uses every 8th data point 
+                for (var i = 0; i<data.list.length; i+=8){
+                    
+                    var forecastDate = $("<div>");
+                    var forecastIcon=$("<img>");
+                    var windForecast=$("<div>"); 
+                    var tempForecast =$("<div>"); 
+                    var humidityForecast=$("<div>");
+                    var oneDayWrapper = $("<div>");
+                    windForecast.text("Wind Speed: " + data.list[i].wind.speed + " mph");
+                    tempForecast.text("Temperature: "+ data.list[i].main.temp + " F");
+                    humidityForecast.text("Humidity: " + data.list[i].main.humidity + " %");
+                    var newdate = new Date(data.list[i].dt * 1000);
+                   
+                    forecastDate.text("("+ (newdate.getMonth()+1) + "/" + newdate.getDate() + "/" + newdate.getFullYear() + ")");
+                    
+                    oneDayWrapper.append(windForecast);
+                    oneDayWrapper.append(tempForecast);
+                    oneDayWrapper.append(humidityForecast);
+                    oneDayWrapper.append(forecastIcon);
+                    oneDayWrapper.append(forecastDate);
+                    $(".five-day-forecast").append(oneDayWrapper);
+    
+                
+                }
+            });
+           
 
         })
     })
@@ -149,11 +142,11 @@ function getData(){
 }
 
 //set data to local storage
-function addInput () {
-    var addedItems = getInput();
+function addInputLocalStorage () {
+    
     cityList = getData();
     
-    if (cityList.includes(cityInput) === false){
+    if (!cityList.includes(cityInput)){
         cityList.push(cityInput);
     }
    
@@ -178,4 +171,3 @@ renderCityList();
 
 // event listener for search button
 $("#searchButton").on('click', addInput);
-$("#searchButton").on('click', getInput);
